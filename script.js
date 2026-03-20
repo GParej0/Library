d5b71adaf6aeb9dc27ae333dc2461c3da79429ec
GParej0 <195317371+GParej0@users.noreply.github.com>
2026-03-20 11:38:16 +0100

    Modify the constructor to a class

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read){
-
+class Book{
+    constructor(title, author, pages, read){
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read){
